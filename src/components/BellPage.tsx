b384ed1e8332f254da271a6f7ab1ebba9d417cf7
LindseyRutledge <67494680+LindseyRutledge@users.noreply.github.com>
2020-12-21 14:18:07 -0800

    Play bell sounds on click.  Attempt at playing locked bell sound on phone shake.

---
+++ src/components/BellPage.tsx
@@ -0,0 +1,142 @@
+import React, { FunctionComponent, useEffect, useState } from 'react';
+// @ts-ignore
+import Shake from 'shake.js';
+import './BellPage.scss';
+
+interface Bell {
+    displayName: string;
+    fileName: string;
+    startTime: number;
+    sharp?: boolean;
+}
+
+export const BellPage: FunctionComponent = () => {
+    const [lockedBellIndex, setLockedBellIndex] = useState<number>();
+
+    useEffect(() => {
+        const shakeEvent = new Shake();
+        shakeEvent.start();
+        // @ts-ignore
+        window.addEventListener('shake', ringBell, false);
+
+        return (() => {
+            // @ts-ignore
+            window.removeEventListener('shake', ringBell, false);
+            shakeEvent.stop();
+        });
+    }, []);
+
+    const bellSounds: HTMLAudioElement[] = bellsCatalog.map((bell: Bell) => {
+        return new Audio(`${process.env.PUBLIC_URL}/sounds/${bell.fileName}`);
+    });
+
+    const ringBell = (index?: number) => {
+        const bellIndex = index ? index : lockedBellIndex;
+
+        if (bellIndex) {
+            const clonedBell: any = bellSounds[bellIndex].cloneNode(); // actually type HTMLAudioElement
+            clonedBell.currentTime = bellsCatalog[bellIndex].startTime;
+            clonedBell.play();
+        }
+    };
+
+    const lockBell = (index: number) => {
+        if (lockedBellIndex === index) {
+            setLockedBellIndex(undefined);
+        } else {
+            setLockedBellIndex(index);
+        }
+    };
+
+    return (
+        <div className='BellPage'>
+            {bellsCatalog.map((bell: Bell, index: number) => {
+                return (
+                    <div
+                        className='BellPage-bell'
+                    >
+                        <div
+                            className={'BellPage-bell-note' + (bell.sharp ? ' sharp' : '')}
+                            onClick={() => ringBell(index)}
+                        >
+                            {bell.displayName}
+                        </div>
+                        <div
+                            className={'BellPage-bell-lock' + (lockedBellIndex === index ? ' locked' : '')}
+                            onClick={() => lockBell(index)}
+                        >
+                            🔒
+                            </div>
+                    </div>
+                );
+            })}
+        </div>
+    );
+};
+
+const bellsCatalog: Bell[] = [
+    {
+        displayName: 'C',
+        fileName: 'low-c.wav',
+        startTime: 0.1
+    },
+    {
+        displayName: 'C♯/D♭',
+        fileName: 'c-sharp-d-flat.wav',
+        startTime: 0.35,
+        sharp: true
+    },
+    {
+        displayName: 'D',
+        fileName: 'd.wav',
+        startTime: 0.25
+    },
+    {
+        displayName: 'D♯/E♭',
+        fileName: 'd-sharp-e-flat.wav',
+        startTime: 0.3,
+        sharp: true
+    },
+    {
+        displayName: 'E',
+        fileName: 'e.wav',
+        startTime: 0.3
+    },
+    {
+        displayName: 'F',
+        fileName: 'f.wav',
+        startTime: 0.35
+    },
+    {
+        displayName: 'G',
+        fileName: 'g.wav',
+        startTime: 0.4
+    },
+    {
+        displayName: 'G♯/A♭',
+        fileName: 'g-sharp-a-flat.wav',
+        startTime: 0.3,
+        sharp: true
+    },
+    {
+        displayName: 'A',
+        fileName: 'a.wav',
+        startTime: 0.3
+    },
+    {
+        displayName: 'A♯/B♭',
+        fileName: 'a-sharp-b-flat.wav',
+        startTime: 0.3,
+        sharp: true
+    },
+    {
+        displayName: 'B',
+        fileName: 'b.wav',
+        startTime: 0.25
+    },
+    {
+        displayName: 'C',
+        fileName: 'high-c.wav',
+        startTime: 0.25
+    }
+];
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BellPage } from './components/BellPage';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BellPage />
   </React.StrictMode>,
   document.getElementById('root')
 );
